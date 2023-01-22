879ab29a237ee32707e2320b02f63912c74f9725
Shabian Arsyl Yonanta <72251541+shabianay@users.noreply.github.com>
2023-01-22 21:41:02 +0700

    update & fixed

---
+++ arsip-main/index.php
@@ -0,0 +1,264 @@
+<?php
+    ob_start();
+    session_start();
+
+    //cek session
+    if(isset($_SESSION['admin'])){
+        header("Location: ./admin.php");
+        die();
+    }
+
+    require_once 'include/config.php';
+    require_once 'include/functions.php';
+    $config = conn($host, $username, $password, $database);
+?>
+<!--
+-->
+<!doctype html>
+<html lang="en">
+
+<!-- Head START -->
+<head>
+    <!-- Meta START -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <?php
+        $query = mysqli_query($config, "SELECT logo from tbl_instansi");
+        list($logo) = mysqli_fetch_array($query);
+        echo '<link rel="shortcut icon" href="upload/'.$logo.'">';
+    ?>
+    <!-- Meta END -->
+
+    <!--[if lt IE 9]>
+    <script src="../asset/js/html5shiv.min.js"></script>
+    <![endif]-->
+
+    <!-- Global style START -->
+    <link type="text/css" rel="stylesheet" href="asset/css/materialize.css">
+
+</head>
+<!-- Head END -->
+
+<!-- Body START -->
+<body class="bg">
+    <div class="container">
+        <div class="login">
+            <form action="">
+                <h1>Login</h1>
+                <hr>
+                <br>
+                <?php
+                        $query = mysqli_query($config, "SELECT * FROM tbl_instansi");
+                        while($data = mysqli_fetch_array($query)){
+                    ?>
+                    <?php
+                        }
+                    ?>
+
+                    <?php
+                        if(isset($_REQUEST['submit'])){
+
+                            //validasi form kosong
+                            if($_REQUEST['username'] == "" || $_REQUEST['password'] == ""){
+                                echo '<div class="upss red-text"><i class="material-icons">error_outline</i> <strong>ERROR!</strong> Username dan Password wajib diisi.
+                                <a class="btn-large waves-effect waves-light red col s11" href="" style="margin: 20px 0 0 5px;"><i class="material-icons md-24">arrow_back</i> Kembali ke login form</a></div>';
+                            } else {
+
+                                $username = trim(htmlspecialchars(mysqli_real_escape_string($config, $_REQUEST['username'])));
+                                $password = trim(htmlspecialchars(mysqli_real_escape_string($config, $_REQUEST['password'])));
+
+                                $query = mysqli_query($config, "SELECT id_user, username, nama, admin FROM tbl_user WHERE username=BINARY'$username' AND password=MD5('$password')");
+
+                                if(mysqli_num_rows($query) > 0){
+                                    list($id_user, $username, $nama, $admin) = mysqli_fetch_array($query);
+
+                                    //buat session
+                                    $_SESSION['id_user'] = $id_user;
+                                    $_SESSION['username'] = $username;
+                                    $_SESSION['nama'] = $nama;
+                                    $_SESSION['admin'] = $admin;
+
+                                    header("Location: ./admin.php");
+                                    die();
+                                } else {
+
+                                    //session error
+                                    $_SESSION['errLog'] = '<center>Username & Password tidak ditemukan!</center>';
+                                    header("Location: ./");
+                                    die();
+                                }
+                            }
+                        } else {
+                    ?>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix md-prefix">account_circle</i>
+                    <input id="username" type="text" class="validate" name="username" required>
+                    <label for="username">Username</label>
+                </div>
+                <br>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix md-prefix">lock</i>
+                    <input id="password" type="password" class="validate" name="password" required">
+                    <label for="password">Password</label>
+                </div>
+                <div class="input-field col s12">
+                    <button type="submit" class="btn-large waves-effect waves-light red col s12" name="submit">LOGIN</button>
+                </div>
+            </form>
+        </div>
+        <div class="right center px-3 justify-content-center mt-4 mb-5">
+        <h4 class="title">PERSMEKENJO</h4>
+            <img class="img-fluid mb-3 d-none d-md-block" src="asset\img\right.png" alt="smklogo">
+        </div>
+    </div>
+</body>
+
+<style>
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+}
+.bg::before {
+    content: '';
+    background-image: url('./asset/img/Background awal.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity: 0.15;
+    filter:alpha(opacity=15);
+    height:100%;
+    width:100%;
+}
+.container {
+    width: 100%;
+    display: flex;
+    max-width: 850px;
+    background: #fff;
+    border-radius: 15px;
+    box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
+}
+
+.login {
+    width: 400px;
+}
+
+.right {
+    margin: 60px auto;
+}
+
+form {
+    width: 250px;
+    margin: 140px auto;
+}
+
+h1 {
+    margin: 20px;
+    text-align: center;
+    text-transform: uppercase;
+}
+
+hr {
+    border-top: 2px solid red;
+}
+
+p {
+    text-align: center;
+    margin: 10px;
+}
+
+form label {
+    display: block;
+    font-size: 16px;
+    font-weight: 600;
+    padding: 5px;
+}
+
+input {
+    width: 100%;
+    margin: 2px;
+    border: none;
+    outline: none;
+    padding: 8px;
+    border-radius: 5px;
+    border: 1px solid gray;
+}
+
+button {
+    border: none;
+    outline: none;
+    padding: 8px;
+    width: 252px;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 20px;
+    border-radius: 5px;
+    background: #ffa12c;
+}
+
+button:hover {
+    background: rgba(214, 86, 64, 1);
+}
+
+
+@media (max-width: 880px) {
+    .container {
+        width: 100%;
+        max-width: 750px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+
+    form {
+        width: 300px;
+        margin: 20px auto;
+    }
+
+    .login {
+        width: 400px;
+        padding: 20px;
+    }
+
+    button {
+        width: 100%;
+    }
+
+    .right img {
+        width: 100%;
+        height: 100%;
+    }
+}
+
+</style>
+<?php
+    }
+?> 
+    <!-- Javascript START -->
+    <script type="text/javascript" src="asset/js/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="asset/js/materialize.min.js"></script>
+    <script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
+    <script data-pace-options='{ "ajax": false }' src='./asset/js/pace.min.js'></script>
+
+    <!-- Jquery auto hide untuk menampilkan pesan error -->
+    <script type="text/javascript">
+        $("#alert-message").alert().delay(3000).slideUp('slow');
+    </script>
+    <!-- Javascript END -->
+
+    <noscript>
+        <meta http-equiv="refresh" content="0;URL='/enable-javascript.html'" />
+    </noscript>
+
+</body>
+<!-- Body END -->
+
+</html>
